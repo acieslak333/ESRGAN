e317f933705ef64827e2f0234a989787fcf7becb
Piotr Cenda <piotr.cenda222@gmail.com>
2022-11-16 14:17:10 +0100

    todo

---
+++ esrgan.py
@@ -0,0 +1,6 @@
+# TODO: 
+# Ogarnąc model pretrained do fine-tune'owania
+# https://github.com/Lornatang/Real_ESRGAN-PyTorch
+# https://github.com/ai-forever/Real-ESRGAN !!
+# https://github.com/johnjaniczek/finetune_ESRGAN/blob/master/finetune_remote.py  !!
+# Ogarnąc różne datasety z MRI (obczaić czy jedna modalność czy wszystko)
